0fd77847b31e71ec1eef018276bcad02a009303d
Kareem Arab <me@kareemarab.com>
2021-12-14 16:27:38 -0500

    added apply page

---
+++ pages/apply.js
@@ -0,0 +1,10 @@
+import Layout from "../../race/components/Layout";
+import ApplicationForm from "../components/ApplicationForm/ApplicationForm";
+
+export default function Apply({}) {
+  return (
+    <Layout>
+      <ApplicationForm />
+    </Layout>
+  );
+}
