d6b9d4c4667f28da37e3b120a6d221e30ac55db9
Kareem Arab <me@kareemarab.com>
2021-12-14 23:15:10 -0500

    removed class tag, formatting

--- components/FileUploader/FileUploader.js
+++ components/FileUploader/FileUploader.js
@@ -4,7 +4,7 @@ const FileUploader = () => {
       <div className="p-4 items-center flex flex-row space-x-3 rounded-lg" style={{ background: "#010C4B0D" }}>
         <img className="ratio ratio-1x1" src="Vector.png"></img>
 
-        <input className="file grow hidden" type="file" id="files" class="hidden" multiple />
+        <input className="file grow hidden" type="file" id="files" multiple />
         <label for="files" style={{ color: "#5F75EE" }}>
           Browse
         </label>
