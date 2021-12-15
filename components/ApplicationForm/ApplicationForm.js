128cc55ccb78c34258ff98a71073a09730ae4735
Kareem Arab <me@kareemarab.com>
2021-12-14 23:06:29 -0500

    formatting

--- components/ApplicationForm/ApplicationForm.js
+++ components/ApplicationForm/ApplicationForm.js
@@ -1,5 +1,5 @@
-import FileUploader from '../FileUploader'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import FileUploader from "../FileUploader";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const ApplicationForm = ({}) => {
   const fakeFiles = ["whitepaper.pdf", "yellowpaper.pdf"];
@@ -8,162 +8,151 @@ const ApplicationForm = ({}) => {
     <div style={{ background: "#46469814" }}>
       <div className="main">
         <div className="w-3/5 mx-auto flex flex-col space-y-10 py-6">
-            <div className="mx-auto flex flex-col space-y-2">
-                <h1 className="mx-auto text-5xl font-extrabold text-gray-900">Enter the DAO race!</h1>
-                <p1 className="mx-auto">Hyperscale Application (next race in x days)</p1>
-            </div>
+          <div className="mx-auto flex flex-col space-y-2">
+            <h1 className="mx-auto text-5xl font-extrabold text-gray-900">Enter the DAO race!</h1>
+            <p1 className="mx-auto">Hyperscale Application (next race in x days)</p1>
+          </div>
 
-            <div className="py-14 px-20 rounded-lg" style={{ background: "#46469814" }}>
-                <Formik
-                initialValues={{ email: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-                >
-
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                }) => (
-                    <form className="flex flex-col space-y-6 ">
-                    <div className="flex flex-col">
+          <div className="py-14 px-20 rounded-lg" style={{ background: "#46469814" }}>
+            <Formik
+              initialValues={{ email: "" }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = "Required";
+                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                  errors.email = "Invalid email address";
+                }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+                <form className="flex flex-col space-y-6 ">
+                  <div className="flex flex-col">
                     <label htmlFor="email" className="mb-2 font-semibold">
-                        Email
+                      Email
                     </label>
                     <input
-                        className="p-6 h-6 border-b-2 rounded-lg"
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
+                      className="p-6 h-6 border-b-2 rounded-lg"
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="project-name" className="mb-2 font-semibold">
-                        Project name
+                      Project name
                     </label>
                     <input
-                        className="p-6 h-6 border-b-2 rounded-lg"
-                        id="project-name"
-                        name="project-name"
-                        type="project-name"
-                        required
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.projectName}
+                      className="p-6 h-6 border-b-2 rounded-lg"
+                      id="project-name"
+                      name="project-name"
+                      type="project-name"
+                      required
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.projectName}
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="pitch-tweet" className="mb-2 font-semibold">
-                        Pitch us your project in a tweet
+                      Pitch us your project in a tweet
                     </label>
                     <textarea
-                        className="p-4 h-36 rounded-lg"
-                        id="pitch-tweet"
-                        name="pitch-tweet"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.pitchTweet}
-                        required
+                      className="p-4 h-36 rounded-lg"
+                      id="pitch-tweet"
+                      name="pitch-tweet"
+                      type="text"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.pitchTweet}
+                      required
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="pitch-full" className="mb-2 font-semibold">
-                        Pitch us your project
+                      Pitch us your project
                     </label>
                     <textarea
-                        className="p-4 h-36 rounded-lg"
-                        id="pitch-full"
-                        name="pitch-full"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.pitchFull}
-                        required
+                      className="p-4 h-36 rounded-lg"
+                      id="pitch-full"
+                      name="pitch-full"
+                      type="text"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.pitchFull}
+                      required
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="founder-background" className="mb-2 font-semibold">
-                        Provide some background on each founder
+                      Provide some background on each founder
                     </label>
                     <textarea
-                        className="p-4 h-36 rounded-lg"
-                        id="founder-background"
-                        name="founder-background"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.founderBackground}
-                        required
+                      className="p-4 h-36 rounded-lg"
+                      id="founder-background"
+                      name="founder-background"
+                      type="text"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.founderBackground}
+                      required
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="founder-ability" className="mb-2 font-semibold">
-                        Please state evidence of exceptional ability for each founder
+                      Please state evidence of exceptional ability for each founder
                     </label>
                     <textarea
-                        className="p-4 h-36 rounded-lg"
-                        id="founder-ability"
-                        name="founder-ability"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.founderAbility}
-                        required
+                      className="p-4 h-36 rounded-lg"
+                      id="founder-ability"
+                      name="founder-ability"
+                      type="text"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.founderAbility}
+                      required
                     />
-                    </div>
+                  </div>
 
-                    <div className="flex flex-col">
+                  <div className="flex flex-col">
                     <label htmlFor="extra-info" className="mb-2 font-semibold">
-                        Is there anything else we should know about?
+                      Is there anything else we should know about?
                     </label>
                     <textarea
-                        className="p-4 h-36 rounded-lg"
-                        id="extra-info"
-                        name="extra-info"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.extraInfo}
-                        required
+                      className="p-4 h-36 rounded-lg"
+                      id="extra-info"
+                      name="extra-info"
+                      type="text"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.extraInfo}
+                      required
                     />
-                    </div>
+                  </div>
 
-                    <div className="space-y-3">
+                  <div className="space-y-3">
                     <div className="flex flex-col">
-                        <label htmlFor="email" className="mb-2 font-semibold">
+                      <label htmlFor="email" className="mb-2 font-semibold">
                         Do you have any links to share?
-                        </label>
-                        <input
+                      </label>
+                      <input
                         placeholder="https://"
                         className="p-6 h-6 rounded-lg"
                         id="link"
@@ -173,96 +162,95 @@ const ApplicationForm = ({}) => {
                         onBlur={handleBlur}
                         value={values.extraInfo}
                         required
-                        />
+                      />
                     </div>
                     <button
-                        className="p-3 mx-auto border border-indigo-600 rounded-xl text-left font-semibold"
-                        style={{ color: "#5F75EE" }}
+                      className="p-3 mx-auto border border-indigo-600 rounded-xl text-left font-semibold"
+                      style={{ color: "#5F75EE" }}
                     >
-                        Add link
+                      Add link
                     </button>
-                    </div>
+                  </div>
 
-                    <div className="space-y-1">
+                  <div className="space-y-1">
                     <div className="flex flex-col">
-                        <label htmlFor="email" className="mb-2 font-semibold">
+                      <label htmlFor="email" className="mb-2 font-semibold">
                         Do you have any files to share?
-                        </label>
+                      </label>
                     </div>
 
                     <FileUploader />
 
                     <div className="flex flex-col">
-                        <label htmlFor="email" className="py-3 mb-2 font-semibold">
+                      <label htmlFor="email" className="py-3 mb-2 font-semibold">
                         Attachments:
-                        </label>
+                      </label>
                     </div>
 
                     <div className="container flex justify-center mx-auto" style={{ background: "green" }}>
-                        <table className="w-1/1 table-fixed hover:table-fixed" style={{ background: "red" }}>
+                      <table className="w-1/1 table-fixed hover:table-fixed" style={{ background: "red" }}>
                         <tbody>
-                            {fakeFiles.forEach((fileName) => {
+                          {fakeFiles.forEach((fileName) => {
                             <div>
-                                <td>{fileName}</td>
-                                <td>delete</td>
+                              <td>{fileName}</td>
+                              <td>delete</td>
                             </div>;
-                            })}
+                          })}
                         </tbody>
-                        </table>
+                      </table>
                     </div>
 
                     <div className="flex flex-col">
-                        <label htmlFor="referral" className="mb-2 font-semibold">
+                      <label htmlFor="referral" className="mb-2 font-semibold">
                         Referral
-                        </label>
-                        <input
+                      </label>
+                      <input
                         placeholder="Optional"
                         className="p-6 h-6 mb-4 border-b-2 rounded-lg"
                         id="referral"
                         name="referral"
                         type="text"
                         required
-                        />
+                      />
                     </div>
 
                     <div className="space-y-8">
-                        <div className="mx-auto flex flex-row space-x-2">
+                      <div className="mx-auto flex flex-row space-x-2">
                         {/* <input className="mx-auto" type="checkbox" className=" checked:bg-blue-500" /> */}
 
                         <label className="flex mx-auto space-x-1">
-                            <input className="ml-2 mr-1 mb-1 mt-1 inline-block" type="checkbox" required />
-                            <span className="text-sm inline-block">
+                          <input className="ml-2 mr-1 mb-1 mt-1 inline-block" type="checkbox" required />
+                          <span className="text-sm inline-block">
                             <div className="flex flex-row space-x-1">
-                                <p1 className="mx-auto font-semibold">I agree to the</p1>{" "}
-                                <a className="font-semibold" href="url" style={{ color: "#5F75EE" }}>
+                              <p1 className="mx-auto font-semibold">I agree to the</p1>{" "}
+                              <a className="font-semibold" href="url" style={{ color: "#5F75EE" }}>
                                 {" "}
                                 Terms{" "}
-                                </a>
-                                <p1 className="font-semibold"> and </p1>
-                                <a className="font-semibold" href="url" style={{ color: "#5F75EE" }}>
+                              </a>
+                              <p1 className="font-semibold"> and </p1>
+                              <a className="font-semibold" href="url" style={{ color: "#5F75EE" }}>
                                 {" "}
                                 Privacy Policy
-                                </a>
+                              </a>
                             </div>
-                            </span>
+                          </span>
                         </label>
-                        </div>
+                      </div>
 
-                        <div className="flex mx-auto">
+                      <div className="flex mx-auto">
                         <button
-                            style={{ background: "#5F75EE", color: "#fff" }}
-                            className="w-2/4 p-3 mx-auto border border-indigo-600 rounded-xl font-bold"
+                          style={{ background: "#5F75EE", color: "#fff" }}
+                          className="w-2/4 p-3 mx-auto border border-indigo-600 rounded-xl font-bold"
                         >
-                            Submit
+                          Submit
                         </button>
-                        </div>
+                      </div>
                     </div>
-                    </div>
-                    </form>
-                )}
-                </Formik>
-
-            </div>
+                  </div>
+                </form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
--- components/FileUploader/FileUploader.js
+++ components/FileUploader/FileUploader.js
@@ -1,14 +1,16 @@
 const FileUploader = () => {
-    return (
-        <>
-        <div className="p-4 items-center flex flex-row space-x-3 rounded-lg" style={{ background: "#010C4B0D" }}>
-            <img className="ratio ratio-1x1" src="Vector.png"></img>
+  return (
+    <>
+      <div className="p-4 items-center flex flex-row space-x-3 rounded-lg" style={{ background: "#010C4B0D" }}>
+        <img className="ratio ratio-1x1" src="Vector.png"></img>
 
-            <input className="file grow hidden" type="file" id="files" class="hidden" multiple />
-            <label for="files" style={{ color: "#5F75EE" }}>Browse</label>
-        </div>
-        </>
-    )
-}
+        <input className="file grow hidden" type="file" id="files" class="hidden" multiple />
+        <label for="files" style={{ color: "#5F75EE" }}>
+          Browse
+        </label>
+      </div>
+    </>
+  );
+};
 
-export default FileUploader
+export default FileUploader;
