3d884a1f50f75fa7060c98f6f0b0330932f4d8d1
Kareem Arab <me@kareemarab.com>
2021-12-19 09:17:38 -0500

    added post api handler skeleton

--- pages/api/applications/index.js
+++ pages/api/applications/index.js
@@ -3,6 +3,8 @@ import { getApplications } from "../../../actions/applications";
 export default async function handler(req, res) {
   if (req.method === "GET") {
     return res.status(200).send(await getApplications());
+  } else if (req.method === "POST") {
+    // handle application upload
   } else {
     res.status(405);
   }
