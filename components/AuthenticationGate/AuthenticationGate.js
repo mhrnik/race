d21d129c67f86f4472bd6cb4f44c281cff904295
Tom König <hi@tomknig.de>
2021-12-21 23:59:52 +0100

    Request user sign in via modal

---
+++ components/AuthenticationGate/AuthenticationGate.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { signIn } from "next-auth/react";
+import Button from "../atoms/Button";
+import Image from "next/image";
+
+function loginWithDiscord() {
+  return signIn("discord");
+}
+
+const AuthenticationGate = ({ userSession, children }) => {
+  const isAuthenticated = userSession?.status === "authenticated";
+
+  return (
+    <>
+      {children}
+      {!isAuthenticated && (
+        <div className="fixed inset-0 w-screen h-screen bg-black/30 flex items-center justify-center">
+          <div className="bg-white rounded-md w-full max-w-md p-12">
+            <h3 className="text-3xl font-extrabold text-gray-900 text-center mb-2">Login to apply</h3>
+            <p className="text-sm text-gray-400 text-center mb-5">Sign in using Discord to apply to Hyperscale.</p>
+            <Button className="w-full flex items-center justify-center" color="primary" onClick={loginWithDiscord}>
+              <Image src="/discord.svg" width="20" height="20" alt="" />
+              <span className="ml-3">Login with Discord</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthenticationGate;
