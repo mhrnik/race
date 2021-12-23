768d46e46e00f1afb32ab5ef7d8b4ae2d9359872
Pawel Kurdybacha <kurdybacha@users.noreply.github.com>
2021-12-23 21:41:21 +0000

    Pass session status instead session to AuthenticationGate
    Problem is that session does not include status, both client and server
    session.

--- pages/join.js
+++ pages/join.js
@@ -1,30 +1,15 @@
 import Layout from "../components/Layout";
 import AuthenticationGate from "../components/AuthenticationGate";
 import ApplicationForm from "../components/ApplicationForm/ApplicationForm";
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
-export default function Join({ userSession }) {
+export default function Join() {
+  const { status: sessionStatus } = useSession();
   return (
-    <AuthenticationGate userSession={userSession}>
+    <AuthenticationGate sessionStatus={sessionStatus}>
       <Layout narrow={false} className="w-screen px-4 xl:px-0 px-4 xl:px-0">
         <ApplicationForm />
       </Layout>
     </AuthenticationGate>
   );
-}
-
-export async function getServerSideProps(context) {
-  let userSession = null;
-
-  try {
-    userSession = await getSession(context);
-  } catch (error) {
-    console.error(error);
-  }
-
-  return {
-    props: {
-      userSession,
-    },
-  };
 }
