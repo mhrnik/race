ee1fa0f2ad98b508ce692467b3a7acb6a90ca5b5
Pawel Kurdybacha <pawel.kurdybacha@gmail.com>
2021-12-29 20:49:38 +0000

    Disable signIn event as user.id is incorrect
    user.id is not the one that is epxected. user.id is accountProviderId
    and proper mongo db user id is not known at this stage. Don't have a
    quick solution for that now so disabling for now.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -55,11 +55,11 @@ export default NextAuth({
           Authorization: `Bearer ${account?.access_token}`,
         },
       });
-      Event.create({
-        type: "signIn",
-        data: { user: user, providerAccountId: account.providerAccountId },
-        userId: user.id,
-      });
+      //Event.create({
+      //  type: "signIn",
+      //  data: { user: user, providerAccountId: account.providerAccountId },
+      //  userId: user.id,
+      //});
       return response.ok;
     },
   },
